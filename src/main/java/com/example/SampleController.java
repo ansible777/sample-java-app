b6feec058ba1e466eb426ce3c2bb5151cc629fdb
keerthisumanth <keerthisumanth.mure@gmail.com>
2019-08-21 13:51:25 +0000

    comrade

--- src/main/java/com/example/SampleController.java
+++ src/main/java/com/example/SampleController.java
@@ -22,7 +22,7 @@ public class SampleController {
     }
     @GetMapping("hai/{name}")
     public ResponseEntity<String> hai(@PathVariable("name") String name){
-        return ResponseEntity.ok("hai......    :     "+name);
+        return ResponseEntity.ok("hai...........    :     "+name);
     }
     @GetMapping("goodMorning/{name}")
     public ResponseEntity<String> goodMorning(@PathVariable("name") String name){
