b195ea7e4b0e6290ee70cc17efc236560cb2c8f8
keerthisumanth <keerthisumanth.mure@gmail.com>
2019-08-26 10:36:14 +0000

    SampleController.java edited online with Bitbucket

--- src/main/java/com/example/SampleController.java
+++ src/main/java/com/example/SampleController.java
@@ -1,8 +1,6 @@
 package com.example;
 
 
-
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
